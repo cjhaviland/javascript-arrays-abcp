b41e4962b951fe5e39052e482f245b03c8771457
CJ Haviland <cj.haviland@yahoo.com>
2018-01-13 16:22:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  var a = array;
+  var a = array.slice();
   a.unshift(element);
   return a;
 }
